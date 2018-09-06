e08e60e2e7226855d2142886aee94902b60899e8
Jeff S <Slater.Jeff7@gmail.com>
2018-09-06 15:37:46 -0600

    CTS coming together working hard coded

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,8 @@
 export const NAVIGATE = 'NAVIGATE'
 export const CHANGE_CTS_VIEW = 'CHANGE_CTS_VIEW'
+export const LOAD_CLIENTS = 'LOAD_CLIENTS'
+export const LOAD_TRAITS = 'LOAD_TRAITS'
+export const LOAD_SURVEYS = 'LOAD_SURVEYS'
 
 export const navigate = destination => {
   return {
@@ -12,5 +15,26 @@ export const changeCTSView = view => {
   return {
     type: CHANGE_CTS_VIEW,
     payload: view
+  }
+}
+
+export const loadClients = () => {
+  return {
+    type: LOAD_CLIENTS,
+    payload: 'clients'
+  }
+}
+
+export const loadTraits = () => {
+  return {
+    type: LOAD_TRAITS,
+    payload: 'traits'
+  }
+}
+
+export const loadSurveys = () => {
+  return {
+    type: LOAD_SURVEYS,
+    payload: 'surveys'
   }
 }
--- src/reducers/mainReducer.js
+++ src/reducers/mainReducer.js
@@ -1,6 +1,9 @@
 import {
   NAVIGATE,
-  CHANGE_CTS_VIEW
+  CHANGE_CTS_VIEW,
+  LOAD_CLIENTS,
+  LOAD_TRAITS,
+  LOAD_SURVEYS
 } from '../actions'
 
 const initialState = {
@@ -23,6 +26,27 @@ export default (state = initialState, action) => {
         ...state,
         CTSView: action.payload
       }
+
+    case LOAD_CLIENTS:
+      return {
+        ...state,
+        // clients: action.payload
+        clients: [{company_name: 'EightTen', first_name: 'rich'}, {company_name: 'Slater Flooring', first_name: 'Jeff'}, {company_name: 'Vape House', first_name: 'Ben'}, {company_name: 'Cow R Us', first_name: 'Peter'}]
+    }
+
+    case LOAD_TRAITS:
+      return {
+        ...state,
+        // traits: action.payload
+        traits: [{trait: 'Employee Impact'}, {trait: 'Community'}, {trait: 'Talent'}]
+    }
+
+    case LOAD_SURVEYS:
+      return {
+        ...state,
+        // surveys: action.payload
+        surveys: action.payload
+    }
 
 
     default: return state
